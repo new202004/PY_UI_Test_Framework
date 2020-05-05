7f6302548e8f6c963f00a17df409e17110d5521b
new202004 <479262985@qq.com>
2020-05-05 22:27:05 +0800

    作业： 2、重新优化元素数据源，添加了 所属页面 之后保证元素信息正常读取

--- common/base_page.py
+++ common/base_page.py
@@ -1,10 +1,13 @@
 import time
+import os
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from common.log_utills import logger
+from common.config_value import config
 
 
 class BasePage:
@@ -13,8 +16,11 @@ class BasePage:
 
     # 浏览器操作封装  -->  二次封装
     def open_url(self, url):
-        self.driver.get(url)
-        logger.info('打开url地址：%s' % url)
+        try:
+            self.driver.get(url)
+            logger.info('打开url地址：%s' % url)
+        except Exception as e:
+            logger.error('不能打开指定的特殊网址，原因是：%s' % e.__str__())
 
     def set_browser_max(self):
         self.driver.maximize_window()
@@ -28,29 +34,49 @@ class BasePage:
         self.driver.refresh()
         logger.info('浏览器刷新')
 
-    def get_title(self, title):
-        value = self.driver.title.text
+    def close_tab(self):
+        self.driver.close()
+        logger.info('关闭当前页面')
+
+    def exit_driver(self):
+        self.driver.quit()
+        logger.info('退出浏览器')
+
+    def get_title(self):
+        value = self.driver.title
         logger.info('获取元素名称:%s' % value)
+        return value
 
     '''self.username_input_box = {'element_name': '用户名输入框','locator_type': 'XPATH',
     'locator_value': '//input[@name="account"]','timeout': 3}'''
     def find_element(self, element_info):
-        locator_element_name = element_info['element_name']
-        locator_type_name = element_info['locator_type']
-        locator_value_info = element_info['locator_value']
-        locator_timeout = element_info['timeout']
+        """
+        根据提供的元素参数信息进行查找
+        :param element_info: 元素信息，字典类型
+        :return: element对象
+        """
+        try:
+            locator_element_name = element_info['element_name']
+            locator_type_name = element_info['locator_type']
+            locator_value_info = element_info['locator_value']
+            locator_timeout = element_info['timeout']
 
-        if locator_type_name == 'id':
-            locator_type = By.ID
-        elif locator_type_name == 'class_name':
-            locator_type = By.CLASS_NAME
-        elif locator_type_name == 'XPATH':
-            locator_type = By.XPATH
+            if locator_type_name == 'id':
+                locator_type = By.ID
+            elif locator_type_name == 'class_name':
+                locator_type = By.CLASS_NAME
+            elif locator_type_name == 'XPATH':
+                locator_type = By.XPATH
 
-        elment = WebDriverWait(self.driver, int(locator_timeout))\
-            .until(lambda x: x.find_element(locator_type, locator_value_info))
-        logger.info('%s：  元素识别成功' % locator_element_name)
-
+            elment = WebDriverWait(self.driver, int(locator_timeout))\
+                .until(lambda x: x.find_element(locator_type, locator_value_info))
+            logger.info('%s：  元素识别成功' % locator_element_name)
+        except Exception as  e:
+            logger.error('%s：  元素识别失败，原因是：%s' % (locator_element_name, e.__str__()))
+            self.screenshot_as_file()
+        # finally:
+        #     if elment is None:
+        #         elment = ''
         return elment
 
         # WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, 'kw')))
@@ -74,6 +100,7 @@ class BasePage:
     def get_text(self, element_info):
         text = self.find_element(element_info).text
         logger.info('%s：  对象的文本信息为【%s】' % (element_info['element_name'], text))
+        return text
 
     # franme处理
     def switch_to_frame(self, frame):
@@ -87,11 +114,21 @@ class BasePage:
         # 包含：frame处理、windows句柄处理、alert处理、鼠标常用操作、键盘常用操作）
 
     # windows句柄处理
+    # 1. 获取当前句柄
+    def get_handle_window(self):
+        return self.driver.current_window_handle
+
+    # 2. 跳转指定句柄
+    def switch_to_window(self, window_handle):
+        return self.driver.switch_to.window(window_handle)
+
+    # 2.切换到指定句柄
     def switch_window_by_title(self, title):
         for handle in self.driver.window_handles:
-            self.driver.switch_to.window(handle)
-            if self.driver.title.__contains__(title):
-                break
+            if WebDriverWait(driver, config.time_out).until(EC.title_contains(title)):
+                self.driver.switch_to.window(handle)
+                if self.driver.title.__contains__(title):
+                    break
         time.sleep(1)
 
     def switch_window_by_url(self, url):
@@ -108,13 +145,18 @@ class BasePage:
         self.driver.execute_script(alert_str)
 
     # 2.切换到js弹窗，获取弹窗提示值，点击确定
-    def switch_to_alert(self):
+    def switch_to_alert(self, action='accept', timeout=int(config.time_out)):
+        WebDriverWait(self.driver, timeout).until(EC.alert_is_present())
         alert = self.driver.switch_to.alert
         text = alert.text
-        alert.accept()
-        logger.info('弹窗提示值为：  %s' % text)
+        if action == 'accept':
+            logger.info('弹窗提示值为：  %s' % text)
+            alert.accept()
+        else:
+            alert.dismiss()
+        return text
 
-    # 鼠标常用操作
+    # 鼠标键盘封装（建议代码思路：判断操作系统类型）
     # 1.鼠标右击
     def mouse_right_click(self, element_info):
         mouse = ActionChains(self.driver)
@@ -125,6 +167,12 @@ class BasePage:
     def mouse_click_and_hold(self, element_info):
         element = self.find_element(element_info)
         ActionChains(self.driver).click_and_hold(element).pause(10).release(element).perform()
+
+    # 3. 鼠标移动到某个元素
+    def mouse_move_to_element(self, element_info):
+        element = self.find_element(element_info)
+        mouse = ActionChains(self.driver)
+        mouse.move_to_element(element).perform()
 
     # 键盘常用操作
     def key_board_operate(self, element, operate):
@@ -137,6 +185,25 @@ class BasePage:
             element.send_keys(Keys.CONTROL, 'c')  # 复制
         elif operate == 'ctrl v':
             element.send_keys(Keys.CONTROL, 'v')  # 粘贴
+
+    # 封装固定等待
+    def wait(self, seconds=config.time_out):
+        time.sleep(seconds)
+
+    # 封装显式等待
+    def time(self, seconds=config.time_out):
+        self.driver.implicitly_wait(seconds)
+
+    # 截图封装
+    def screenshot_as_file(self, *screenshot_path):
+        current_dir = os.path.dirname(__file__)
+        if len(screenshot_path) == 0:
+            screenshot_file_path = config.screen_shot_path
+        else:
+            screenshot_file_path = screenshot_path[0]
+        now = time.strftime('%Y_%m_%d_%H_%M_%S')
+        screenshot_file_path = os.path.join(current_dir, screenshot_file_path, 'UI_test_%s.png' % now)
+        self.driver.get_screenshot_as_file(screenshot_file_path)
 
 
 if __name__ == '__main__':
